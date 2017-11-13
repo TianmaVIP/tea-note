5ec63c56fb039c60bed78e1c2e5afd8f50731b69
TianmaVIP <32473664+TianmaVIP@users.noreply.github.com>
2017-11-13 22:29:35 +0800

    last commit

--- tea-note/ChkLogin.php
+++ tea-note/ChkLogin.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>无标题文档</title>
+<title></title>
 </head>
 <body>
 <?php
@@ -18,7 +18,10 @@ if($role=="teacher")
 	}
 	else
 	{
-		$ChkLogin="select * from student where StuNo='$username' and Pwd='$userpwd'";
+		echo"<script>";
+					echo"alert(\"暂无学生管理系统，请期待后续升级加入\");";
+					echo"location.href=\"login.php\"";
+					echo"</script>";
 		}
 		$ChkLoginResult=db_query($ChkLogin);
 		$number=db_num_rows($ChkLoginResult);
@@ -30,7 +33,10 @@ if($role=="teacher")
 				header("Location:tealog/main.php");
 				}else{
 					$_SESSION["username"]=$username;
-					header("Location:stu/ShowCourse.php");
+					echo"<script>";
+					echo"alert(\"暂无学生管理系统，请期待后续升级加入\");";
+					echo"location.href=\"login.php\"";
+					echo"</script>";
 					//echo "123";
 					}	
 			}else{
--- tea-note/modifypassword.php
+++ tea-note/modifypassword.php
@@ -1,41 +1,46 @@
-<!doctype html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8">
-<title>首页</title>
-<link href="css/modifypassword.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>网站后台管理系统</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/jquery.js"></script>
 
 </head>
 
 <body>
-<header>
-    <aside><img src="images/logo.PNG" /></aside>
-
-</header>
-<div id="content">
+<div class="place">
+    <span>位置：</span>
+    <ul class="placeul">
+    
+    <li><a href="#">查找信息</a></li>
+    <li><a href="#">数据列表</a></li>
+    </ul>
+    </div>
+<div id="content"  align:"center";>
 	<form method="post" action="changepassword.php">
-    <table  align="center" valign="middle" border="0" cellpadding="0" cellspacing="0" >
+    <table  align="center" valign="middle" border="1" cellpadding="0" cellspacing="0" >
       
       <tr>
-        <td width="69">用户名：</td>
+        <td >用户名：</td>
         <td width="142"><label for="textfield"></label>
-        <input name="username" type="text" id="textfield" size="15" /></td>
+        <input name="username" type="text" id="textfield" size="15" /><br/></td>
       </tr>
       <tr>
         <td>原密码：</td>
-        <td><label for="textfield2"></label>
-        <input name="userpwd" type="password" id="textfield2" size="15" /></td>
+        <td width="142"><label for="textfield2"></label>
+        <input name="userpwd" type="password" id="textfield2" size="15" /><br/></td>
 	  </tr>
 	  <tr>
 		<td>现密码：</td>
-        <td><label for="textfield3"></label>
+        <td width="142"><label for="textfield3"></label>
         <input name="newpwd" type="password" id="textfield3" size="15" /></td>
 		
       <tr>
         <td>身份：</td>
         <td><label for="select">
           <select name="role" size="1">
-            <option value="student">学生</option>
+            
             <option value="teacher">教师</option>
           </select>
 		  <a href='login.php'>返回登陆</a>
@@ -54,8 +59,6 @@
   </form>
     </div>
     <div class="clear"></div>
-    <footer>
-    	<p>版权所有 北工院2017</p>
-    </footer>
+   
 </body>
 </html>
